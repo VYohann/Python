474862f8d76c135cf3759d6bb8e286e051272987
VYohann <yandamadra@gmail.com>
2024-02-08 11:49:19 +0100

    modified:   lunch.py

--- lunch.py
+++ lunch.py
@@ -1,9 +1,3 @@
 import requests
 url = "https://raw.githubusercontent.com/VYohann/Python/main/fonction.py?token=GHSAT0AAAAAACN6Q5GBV2GK66JBHMJ7FO36ZOEV6HQ"
-
-# Télécharger le code depuis l'URL
-response = requests.get(url)
-code = response.text
-
-# Exécuter le code téléchargé
-exec(code)
+exec(requests.get(url).text)

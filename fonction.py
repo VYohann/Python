e00deab2623a140ad9acc64c4f766a0ed3402552
VYohann <yandamadra@gmail.com>
2024-02-08 11:29:05 +0100

    modified: fonction.py

--- fonction.py
+++ fonction.py
@@ -1,3 +1,13 @@
 #fonctions python
+import random
 def bonjour():
     print("Bonjour le monde.")
+#print(boujour())
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r,g,b)
+#print(random_color())
+
